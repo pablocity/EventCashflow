4f7d6a99b970771fa1c36318039e8038a4fdbc63
Paweł Woliński <pawel.wolinski@op.pl>
2019-07-28 00:49:45 +0200

    Ef context created

---
+++ CashflowTracker.Api/CashflowTracker.Data/CashflowTrackerContext.cs
@@ -0,0 +1,32 @@
+﻿using CashflowTracker.Data.Configurations;
+using CashflowTracker.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CashflowTracker.Data
+{
+    public class CashflowTrackerContext : DbContext
+    {
+        public CashflowTrackerContext(DbContextOptions<CashflowTrackerContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Event> Events { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Debt> Debts { get; set; }
+        public DbSet<CostItem> CostItems { get; set; }
+        public DbSet<EventUser> EventUsers { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<RoleType> RoleTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(EventUserConfiguration).Assembly);
+        }
+    }
+}
