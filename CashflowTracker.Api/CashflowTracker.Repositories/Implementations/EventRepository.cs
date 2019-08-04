8409ae2b194f1c9dc16836f179446037a6e3f089
Paweł Woliński <pawel.wolinski@op.pl>
2019-08-04 23:41:51 +0200

    Added Event repository

---
+++ CashflowTracker.Api/CashflowTracker.Repositories/Implementations/EventRepository.cs
@@ -0,0 +1,19 @@
+﻿using CashflowTracker.BaseRepositories.Implementations;
+using CashflowTracker.Data;
+using CashflowTracker.Models;
+using CashflowTracker.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CashflowTracker.Repositories.Implementations
+{
+    public class EventRepository : Repository<Event, CashflowTrackerContext, long>, IEventRepository
+    {
+        public EventRepository(CashflowTrackerContext context)
+            : base(context, entity => entity.Id)
+        {
+
+        }
+    }
+}
---
+++ CashflowTracker.Api/CashflowTracker.Repositories/Interfaces/IEventRepository.cs
@@ -0,0 +1,12 @@
+﻿using CashflowTracker.BaseRepositories.Interfaces;
+using CashflowTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CashflowTracker.Repositories.Interfaces
+{
+    public interface IEventRepository : IRepository<Event>
+    {
+    }
+}
