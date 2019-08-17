6c8fa6210a1360a72419ebe8d23cc0ace595cc93
Paweł Woliński <pawel.wolinski@op.pl>
2019-08-17 23:26:25 +0200

    Added base results, dtos folder, handlers project

---
+++ CashflowTracker.Api/CashflowTracker.Handlers/HandlersAssemblyAnchor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CashflowTracker.Handlers
+{
+    public class HandlersAssemblyAnchor
+    {
+        //Empty class to mark assembly
+    }
+}
---
+++ CashflowTracker.Common/CashflowTracker.Contracts/Queries/Response.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CashflowTracker.Contracts.Queries
+{
+    public class Response<TEntity>
+    {
+        public TEntity Result { get; set; }
+        public Dictionary<long, string> Errors { get; set; } = new Dictionary<long, string>();
+    }
+}
